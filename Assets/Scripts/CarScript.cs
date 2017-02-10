811535ff7758dcd1d16f0e65182eba1682beaaa3
Hannah <hannahbdadd@googlemail.com>
2017-02-10 23:48:04 +0000

    car script

---
+++ Assets/Scripts/CarScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour {
+    int time;
+    public float speedModifier;
+    private float speed;
+    Vector3 startPlace;
+
+    // Use this for initialization
+    void Start () {
+        time = 0;
+        speed = 3;
+        startPlace = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speedModifier * speed * Time.deltaTime, 0, 0);
+
+
+        if (time > 1000)
+        {
+            time = 0;
+            transform.position = startPlace;
+        }
+        time++;
+
+    }
+}
