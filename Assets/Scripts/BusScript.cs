91d05c9ca626e97b2e1b905f649bd87393f5b123
cherwil <wchering@gmail.com>
2017-02-10 23:11:17 +0000

    Merge branch 'master' into sprite_animation
    # Conflicts:
    #	Assets/Scenes/main.unity

---
+++ Assets/Scripts/BusScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BusScript : MonoBehaviour
+{
+    int time;
+    public float speedModifier;
+    private float speed;
+    Vector3 startPlace;
+
+    // Use this for initialization
+    void Start()
+    {
+        time = 0;
+        speed = 3;
+        startPlace = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(speedModifier  * speed * Time.deltaTime, 0, 0);
+        time++;
+        if(time == 1)
+        {
+            speed = 3;
+        }
+        if (time == 200)
+        {
+            speed = 0;
+        }
+        if (time == 350)
+        {
+            speed = 3;
+        }
+        if (time == 650)
+        {
+            speed = 0;
+            transform.position = startPlace;
+        }
+        if(time == 800)
+        {
+            time = 0;
+        }
+    }
+}
