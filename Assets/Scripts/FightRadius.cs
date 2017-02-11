4eb8a5100a74317334ed7125379badc923b4de33
Aleksis Andzans <0andza31@solent.ac.uk>
2017-02-11 16:04:56 +0000

    player must hold key to break up fights

--- Assets/Scripts/FightRadius.cs
+++ Assets/Scripts/FightRadius.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FightRadius : MonoBehaviour {
+    [SerializeField] float breakUpHoldTime;//time player must hold F to break up a fight
+
     Person person;// the person this radius belongs to
+    float fHeldTime = 0f;// time player has held F
+    bool beingStopped = false;// is the player stopping a fight here now?
 
     public SpriteRenderer sr;//renderer for key prompt sprite
     public CircleCollider2D col;//this object's collider
@@ -17,11 +21,39 @@ public class FightRadius : MonoBehaviour {
 
     void Update()
     {
-        if (sr.enabled && Input.GetKeyDown(KeyCode.F) && person.fighting != null)
+        if (sr.enabled && person.fighting != null)
         {
-            // player breaks up fight
-            person.fighting.StopFighting();
-            person.StopFighting();
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                beingStopped = true;
+                fHeldTime = 0f;// reset timer
+            }
+            if (beingStopped)
+            {
+                if (fHeldTime >= breakUpHoldTime)
+                {
+                    // player breaks up fight
+                    person.fighting.StopFighting();
+                    person.StopFighting();
+                    fHeldTime = 0f;// reset timer
+                    beingStopped = false;
+                }
+                else if (Input.GetKey(KeyCode.F))
+                {
+                    // increment timer
+                    fHeldTime += Time.deltaTime;
+                }
+                else
+                {
+                    fHeldTime = 0f;// reset timer
+                    beingStopped = false;
+                }
+            }
+        }
+        else
+        {
+            fHeldTime = 0f;// reset timer
+            beingStopped = false;
         }
     }
 
@@ -51,6 +83,9 @@ public class FightRadius : MonoBehaviour {
         {
             // disable prompt
             sr.enabled = false;
+
+            fHeldTime = 0f;// reset timer
+            beingStopped = false;
         }
     }
 }
