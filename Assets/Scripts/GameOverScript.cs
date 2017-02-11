fdaf43d11ae33cf156f47b0d5d59c2434999ac85
S Williams <scottwill@tesco.net>
2017-02-11 12:16:55 +0000

    stuff

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public Texture gameOverTexture;
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), gameOverTexture);
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 150, 25), "Don't try again, you are going to lose it anyway, because you are a LOOSER!"))
+        {
+            SceneManager.LoadScene("Main");
+        }
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 + 25, 150, 25), "Run away, you coward! That's the only thing that you can do here."))
+        {
+            Application.Quit();
+        }
+    }
+}
