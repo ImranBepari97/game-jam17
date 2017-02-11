6ae8c5870f144f07fd7189247670bd0d17c45bf7
S Williams <scottwill@tesco.net>
2017-02-11 15:08:51 +0000

    GUI stuff

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour {
+
+    public Text textField;
+    private int score;
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        score = score + 1;
+        UpdateScore();
+	}
+
+    void addScore(int n)
+    {
+        score = score + n;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        textField.text = "Score: " + score;
+    }
+}
