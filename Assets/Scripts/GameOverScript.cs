872d5311a0541af76fc1e97f04fafe9f6c2cf476
S Williams <scottwill@tesco.net>
2017-02-11 12:31:44 +0000

    game over thing

--- Assets/Scripts/GameOverScript.cs
+++ Assets/Scripts/GameOverScript.cs
@@ -20,11 +20,11 @@ public class GameOverScript : MonoBehaviour {
     void OnGUI()
     {
         GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), gameOverTexture);
-        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 150, 25), "Don't try again, you are going to lose it anyway, because you are a LOOSER!"))
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, 150, 25), "Try Again."))
         {
             SceneManager.LoadScene("Main");
         }
-        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 + 25, 150, 25), "Run away, you coward! That's the only thing that you can do here."))
+        if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2 + 25, 150, 25), "Quit."))
         {
             Application.Quit();
         }
