1761927ee9914153c0b4a823fa19f60def4afd10
Aleksis Andzans <0andza31@solent.ac.uk>
2017-02-11 02:43:17 +0000

    calculation of whether person starts fighting; stopped tracking some Library files

--- Assets/Scripts/Person.cs
+++ Assets/Scripts/Person.cs
@@ -5,24 +5,53 @@ using UnityEngine;
 public class Person : MonoBehaviour {
 
 
-    [SerializeField] int currentDrunkness;
-    [SerializeField] int minDrunkness;
-    [SerializeField] int maxDrunkness;
-    [SerializeField] int minView;
-    [SerializeField] int maxView;
-    [SerializeField] int view;
-    
+    [SerializeField]
+    int currentDrunkness;
+    [SerializeField]
+    int minDrunkness;
+    [SerializeField]
+    int maxDrunkness;
+    [SerializeField]
+    int minView;
+    [SerializeField]
+    int maxView;
+    [SerializeField]
+    float baseDrunkChance; //base probability of starting a fight when at minDrunkness
+    [SerializeField]
+    float probabilityScale;//multiply fight probability by this amount every check
 
+    CircleCollider2D fightCollider;
+
+    public int view;
+    public Person fighting = null;//who is this person fighting right now?
+
+    void Awake()
+    {
+        fightCollider = GetComponentInChildren<CircleCollider2D>();
+    }
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
         //sets drunkness and view values in the given range
         currentDrunkness = Random.Range(minDrunkness, maxDrunkness);
         view = Random.Range(minView, maxView);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Calculate the probability of starting a fight with another person.
+	public void CheckStartFight(Person other)
+    {
+        if (fighting == null)
+        {
+            float distChance = 1 - Vector2.Distance(transform.position, other.transform.position) / fightCollider.radius;
+            float viewChance = (float)Mathf.Abs(view - other.view) / (maxView - minView);
+            float drunkChance = baseDrunkChance + (float)currentDrunkness / (maxDrunkness - minDrunkness) * (1 - baseDrunkChance);
+            float chance = 1 - Mathf.Pow(1 - distChance * viewChance * drunkChance, Time.fixedDeltaTime);
+            if (Random.value < chance)
+            {
+                fighting = other;
+                other.fighting = this;
+            }
+        }
+    }
 }
