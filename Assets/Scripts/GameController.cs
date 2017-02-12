d7768ff7bf741e53527c8846cbf07ed6b782e5f1
Imran Bepari <imranbepari97@gmail.com>
2017-02-12 10:47:50 +0000

    Fixed Score and made Revolution increase

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,26 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour {
 
     [SerializeField]
     public int score;
-    
 
+    [SerializeField]
+    public int revolution;
+    public int maxRevolution;
+
+    [SerializeField]
+    public float revolutionGain;
+    [SerializeField]
+    Text scoreText;
 
 
 	// Use this for initialization
 	void Start () {
         score = 0;
+        revolution = 0;
+        StartCoroutine(addRevolution());
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        
+        scoreText.text = "Score: " + score;
 	}
 
     void endGame()
     {
         SceneManager.LoadScene("GameOver");
+    }
+
+    IEnumerator addRevolution()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(revolutionGain);
+            revolution++;
+        }
     }
 }
